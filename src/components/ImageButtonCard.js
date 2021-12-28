db23c77e6772024372a2a82b3130367f2aaa873d
Sebastian Abril <sebastianabril6@gmail.com>
2021-12-28 16:16:26 -0500

    Reworked code in order to be more cohesive

--- src/components/ImageButtonCard.js
+++ src/components/ImageButtonCard.js
@@ -1,6 +1,7 @@
 import { Column } from "./Column.js";
 import { Label } from "./Label.js";
-import { Button } from "./Button.js";
+import { Button } from "./button/Button";
+
 /*
  props = {
      style: {
@@ -13,19 +14,22 @@ import { Button } from "./Button.js";
  }
  */
 
+export const ImageButtonCard = ({
+  style = {},
+  buttonType = "fill",
+  buttonIcon = "arrow-right",
+}) => {
+  const imageButtonCard = Column({
+    className: "imageButtonCard",
+    style,
+    children: [
+      Label({ text: "Title" }),
+      Label({ text: "Description" }),
+      Button({ text: "Button", type: buttonType, icon: buttonIcon }),
+    ],
+  });
 
-export const ImageButtonCard = ({ style = {}, buttonType = 'fill', buttonIcon = 'arrow-right'}) => {
-    const imageButtonCard = Column({
-        className: 'imageButtonCard',
-        style,
-        children: [
-            Label({ text: 'Title'}),
-            Label({ text: 'Description'}),
-            Button({ text: 'Button', type: buttonType, icon: buttonIcon}),
-        ]
-    });
+  imageButtonCard.style.backgroundImage = "url(../icons/Block.svg)";
 
-    imageButtonCard.style.backgroundImage= 'url(../icons/Block.svg)'
-
-    return imageButtonCard;
+  return imageButtonCard;
 };
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-
+import "../css/styles.css";
 import { HomeScreen } from "./screens/HomeScreen.js";
 
-const app = document.getElementById('app');
+const app = document.getElementById("app");
 //const screen = ComponentScreen();
 const screen = HomeScreen();
 app.append(screen);

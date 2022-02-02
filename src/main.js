d0f4c7dbaa3001b2e055122ec20e3b511dafe387
Juan Sebastian Abril <sebastianabril6@gmail.com>
2022-02-02 07:39:05 -0500

    last changes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import "../css/styles.css";
-import { HomeScreen } from "./screens/HomeScreen.js";
+//import { HomeScreen } from "./screens/HomeScreen.js";
+import { ProductCardScreen } from "./screens/ProductCardScreen.js";
 
 const app = document.getElementById("app");
 //const screen = ComponentScreen();
-const screen = HomeScreen();
+//const screen = HomeScreen();
+const screen = ProductCardScreen();
 app.append(screen);
